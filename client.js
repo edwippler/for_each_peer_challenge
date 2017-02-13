cc29f42698d2b66b1f5ec1e2be782c782ce66e9f
Edmund Wippler <edmund@Edmunds-MacBook-Pro.local>
2017-02-13 16:29:11 -0600

    Pro Mode completed w/ bonus style sheet

--- client.js
+++ client.js
@@ -1,19 +1,46 @@
 var personArray = [{name: 'Fred', yearsOfExperience: 2}, {name: 'George', yearsOfExperience: 3}, {name: 'Betty', yearsOfExperience: 4}];
 var combinedExperience = 0;
+function Object(name, years){
+  this.name = name;
+  this.yearsOfExperience = parseInt(years);
+}
 
 $(document).ready(function(){
-// read each item in array - isolates years of experience
-personArray.forEach(function(employee){
-  var name = employee.name;
-  var years = employee.yearsOfExperience;
-  $('#employeeTable').append('<tr>' +
+
+  // read each item in array - isolates years of experience
+  personArray.forEach(function(employee){
+    var name = employee.name;
+    var years = employee.yearsOfExperience;
+    $('#employeeTable').append('<tr>' +
     '<td>' + name + '</td>'+
     '<td>'+ years + '</td>'+
-  '</tr>');
+    '</tr>');
 
-  combinedExperience += years;
-  $('#totalYears').text(combinedExperience); 
-})
-// console.log(combinedExperience);
+    combinedExperience += years;
+    $('#totalYears').text(combinedExperience);
+  })
+  // console.log(combinedExperience);
+  $('#submit').on('click', function(){
+    var newName = $('#newEmployeeName').val();
+    var newYearsExperience = $('#newEmployeeYearsOfExperience').val();
+    personArray.push(new Object(newName,newYearsExperience));
+    // console.log(personArray);
+    $('#employeeTable').find('tr').remove();
+    combinedExperience = 0;
+    $('.entryFields').val(''); 
 
+    personArray.forEach(function(employee){
+      var name = employee.name;
+      var years = employee.yearsOfExperience;
+      $('#employeeTable').append('<tr>' +
+      '<td>' + name + '</td>'+
+      '<td>'+ years + '</td>'+
+      '</tr>');
+
+      combinedExperience += years;
+      $('#totalYears').text(combinedExperience);
+
+    });
+
+  });
 });
