5e0003de77ab2447357ca12c4a1a2cb42ed4166b
Edmund Wippler <edmund@Edmunds-MacBook-Pro.local>
2017-02-14 10:28:04 -0600

    refactored using .serializeArray

--- client.js
+++ client.js
@@ -1,13 +1,7 @@
 var personArray = [{name: 'Fred', yearsOfExperience: 2}, {name: 'George', yearsOfExperience: 3}, {name: 'Betty', yearsOfExperience: 4}];
 var combinedExperience = 0;
-function Object(name, years){
-  this.name = name;
-  this.yearsOfExperience = parseInt(years);
-}
 
 $(document).ready(function(){
-
-  // read each item in array - isolates years of experience
   personArray.forEach(function(employee){
     var name = employee.name;
     var years = employee.yearsOfExperience;
@@ -15,32 +9,31 @@ $(document).ready(function(){
     '<td>' + name + '</td>'+
     '<td>'+ years + '</td>'+
     '</tr>');
-
     combinedExperience += years;
     $('#totalYears').text(combinedExperience);
   })
-  // console.log(combinedExperience);
-  $('#submit').on('click', function(){
-    var newName = $('#newEmployeeName').val();
-    var newYearsExperience = $('#newEmployeeYearsOfExperience').val();
-    personArray.push(new Object(newName,newYearsExperience));
-    // console.log(personArray);
-    $('#employeeTable').find('tr').remove();
-    combinedExperience = 0;
-    $('.entryFields').val(''); 
 
-    personArray.forEach(function(employee){
-      var name = employee.name;
-      var years = employee.yearsOfExperience;
-      $('#employeeTable').append('<tr>' +
-      '<td>' + name + '</td>'+
-      '<td>'+ years + '</td>'+
-      '</tr>');
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var submissionArray = $(this).serializeArray();
+    var newEmployeeObject = {};
 
-      combinedExperience += years;
-      $('#totalYears').text(combinedExperience);
-
+    submissionArray.forEach(function(inputField){
+      newEmployeeObject[inputField.name] = inputField.value;
     });
+    personArray.push(newEmployeeObject);
+    // console.log(newEmployeeObject);
+    // console.log(personArray);
+    $('#employeeTable').append('<tr>' +
+    '<td>' + newEmployeeObject.newEmployeeName + '</td>'+
+    '<td>'+ newEmployeeObject.newEmployeeYearsOfExperience + '</td>'+
+    '</tr>');
+
+    combinedExperience += Number(newEmployeeObject.newEmployeeYearsOfExperience);
+    $('#totalYears').text(combinedExperience);
+
+    $('.entryFields').val('');
+
 
   });
 });
